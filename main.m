8dea86395f5a75e752ba0154c66338eab5a62c60
SanderMiller <42980999+SanderMiller@users.noreply.github.com>
2020-04-26 19:41:22 -0400

    Moved Graph Visualization

--- main.m
+++ main.m
@@ -7,6 +7,19 @@ addpath(fullfile(curr_dir, "src"));
 [imgs, lbls] = load_imgs_std_sz_lbls(data_folder, "*.jpg", [500, 500]);
 
 %% Visualizing Graphs With Random Image Rotations
+<<<<<<< HEAD
+rgbIm = squeeze(imgs(4, :, :, :));
+grayIm = rgb2gray(rgbIm);
+
+[G, points] = image2Graph(grayIm, 100, 100);
+
+subplot(1,4,1)
+imshow(grayIm)
+
+
+subplot(1,4,2)
+p1 = plot(G,'XData',points(:,1),'YData',points(:,2));
+=======
 figure(1);
 for i = 1:2*length(lbls)
 rgbIm = squeeze(imgs(ceil(i/2), :, :, :));
@@ -19,26 +32,20 @@ if i == 1
 end
 subplot(2*length(lbls),4,i*4-2)
 p1 = plot(G,'XData',normalizedPoints(:,1),'YData',normalizedPoints(:,2));
+>>>>>>> 593ca7a4d892801224ba4405c3890d00f598031c
 p1.NodeColor = 'r';
 p1.NodeLabel = [];
-if i == 1
-   title('In Place Graph')
-end
-subplot(2*length(lbls),4,i*4-1)
+
+subplot(1,4,3)
 p2 = plot(G,'Layout','force');
 p2.NodeColor = 'r';
 p2.NodeLabel = [];
-if i == 1
-   title('Expanded Graph')
-end
-subplot(2*length(lbls),4,i*4)
-L = laplacian(G);
-e = eig(L);
-histogram(e, length(unique(e)));
-if i == 1
-   title('Eigen Values');
-end
-end
 
-
-
+subplot(1,4,4)
+D = diag(degree(G));
+L = full(laplacian(G));
+D = D.^-.5;
+D(~isfinite(D)) = 0;
+symL = D*L*D;
+e = eig(symL);
+histogram(e, 'BinEdges',-0:.02:2);
