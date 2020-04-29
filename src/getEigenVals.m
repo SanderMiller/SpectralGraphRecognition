e343afbc237f6d0cc0560660bad441b02b4a7243
Duncan Mazza <dmazza@olin.edu>
2020-04-29 12:49:45 -0600

    Update function code style for best practice

--- src/getEigenVals.m
+++ src/getEigenVals.m
@@ -1,5 +1,8 @@
 function [eigenValues] = getEigenVals(G)
-D = diag(degree(G)).^(-.5);
-D(~isfinite(D)) = 0;
-eigenValues = eig(D*full(laplacian(G))*D);
+    %GETEIGENVALS Summary of this function goes here
+    %   Detailed explanation goes here
+
+    D = diag(degree(G)).^(-.5);
+    D(~isfinite(D)) = 0;
+    eigenValues = eig(D*full(laplacian(G))*D);
 end
--- src/image2Graph.m
+++ src/image2Graph.m
@@ -1,7 +1,11 @@
 function [grph, normalizedPoints, points] = image2Graph(grayImage, maxNodes, distanceThreshold)
-[theta, rho] = image2HoughPeaks(grayImage, 0.4, maxNodes);
-points = [theta', rho'];
-[normalizedTheta, shiftedRho] = houghPeakNormalization(theta, rho);
-normalizedPoints = [normalizedTheta',shiftedRho'];
-Adjacency = squareform(pdist(normalizedPoints)) < distanceThreshold - eye(length(normalizedPoints));
-grph = graph(Adjacency);
+    %IMAGE2GRAPH Summary of this function goes here
+    %   Detailed explanation goes here
+    
+    [theta, rho] = image2HoughPeaks(grayImage, 0.4, maxNodes);
+    points = [theta', rho'];
+    [normalizedTheta, shiftedRho] = houghPeakNormalization(theta, rho);
+    normalizedPoints = [normalizedTheta',shiftedRho'];
+    Adjacency = squareform(pdist(normalizedPoints)) < distanceThreshold - eye(length(normalizedPoints));
+    grph = graph(Adjacency);
+end
