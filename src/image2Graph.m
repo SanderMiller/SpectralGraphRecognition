90df4bccd0c4333a1e60b1381db2d7b34005c945
Duncan Mazza <dmazza@olin.edu>
2020-04-29 12:35:05 -0600

    Make adjacency matrix a logical array instead of weighted edges

--- src/image2Graph.m
+++ src/image2Graph.m
@@ -1,15 +1,7 @@
 function [grph, normalizedPoints, points] = image2Graph(grayImage, maxNodes, distanceThreshold)
-
 [theta, rho] = image2HoughPeaks(grayImage, 0.4, maxNodes);
 points = [theta', rho'];
-
 [normalizedTheta, shiftedRho] = houghPeakNormalization(theta, rho);
-
 normalizedPoints = [normalizedTheta',shiftedRho'];
-D = pdist(normalizedPoints);
-Dist = squareform(D);
-Adjacency = Dist.^-1;
-Adjacency(Adjacency<(1/distanceThreshold)) = 0;
-Adjacency(Adjacency==Inf) = 0;
-
+Adjacency = squareform(pdist(normalizedPoints)) > distanceThreshold;
 grph = graph(Adjacency);
