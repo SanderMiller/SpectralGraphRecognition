b43f36d0fd8a79bd0ecead2fe2e8fbf5748d92c3
Duncan Mazza <dmazza@olin.edu>
2020-05-02 12:50:59 -0600

    Fix mismatched parentheses

--- src/image2Graph.m
+++ src/image2Graph.m
@@ -6,6 +6,6 @@ function [grph, normalizedPoints, points] = image2Graph(grayImage, maxNodes, dis
     points = [theta', rho'];
     [normalizedTheta, shiftedRho] = houghPeakNormalization(theta, rho);
     normalizedPoints = [normalizedTheta',shiftedRho'];
-    grph = graph(squareform(pdist(normalizedPoints)) < ...
-        distanceThreshold) - eye(length(normalizedPoints));
+    grph = graph((squareform(pdist(normalizedPoints)) < ...
+        distanceThreshold) - eye(length(normalizedPoints)));
 end
