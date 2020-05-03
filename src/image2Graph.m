5c2dda9530280e125b5df00b815167335364edcc
Alexander Miller <amiller@olin.edu>
2020-05-03 13:51:24 -0400

    Implementing 90 degree shift

--- src/image2Graph.m
+++ src/image2Graph.m
@@ -1,11 +1,18 @@
-function [grph, normalizedPoints, points] = image2Graph(grayImage, maxNodes, distanceThreshold)
+function [grph] = image2Graph(grayImage, maxNodes, distanceThreshold)
     %IMAGE2GRAPH Summary of this function goes here
     %   Detailed explanation goes here
     
     [theta, rho] = image2HoughPeaks(grayImage, 0.4, maxNodes);
-    points = [theta', rho'];
-    [normalizedTheta, shiftedRho] = houghPeakNormalization(theta, rho);
-    normalizedPoints = [normalizedTheta',shiftedRho'];
-    grph = graph(squareform(pdist(normalizedPoints)) < ...
+    origPoints = [theta', rho'];
+    shiftedTheta = theta-90
+    wrappedPoints = ones(1,length(shiftedTheta)); 
+    wrappedPoints = wrappedPoints - (2.*((shiftedTheta()<-90))); 
+    shiftedTheta(shifteTheta<-90) = shiftedTheta(shiftedTheta<-90)+180; 
+    shiftedRho = rho.*wrappedPoints;
+    shiftedPoints = [shiftedTheta',shiftedRho']
+    dist1 = squareForm(pdist(origPoints))
+    dist2 = squareForm(pdist(shiftedPoints))
+    shortestDist = min(dist1,dist2)
+    grph = graph(shortestDist < ...
         distanceThreshold) - eye(length(normalizedPoints));
 end
