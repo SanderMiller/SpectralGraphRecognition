634b303d64caf3468fa45d857cb9a03e9a687b01
Alexander Miller <amiller@olin.edu>
2020-05-03 13:53:53 -0400

    Fixing Merge

--- src/image2Graph.m
+++ src/image2Graph.m
@@ -3,7 +3,6 @@ function [grph] = image2Graph(grayImage, maxNodes, distanceThreshold)
     %   Detailed explanation goes here
     
     [theta, rho] = image2HoughPeaks(grayImage, 0.4, maxNodes);
-<<<<<<< HEAD
     origPoints = [theta', rho'];
     shiftedTheta = theta-90
     wrappedPoints = ones(1,length(shiftedTheta)); 
@@ -16,11 +15,5 @@ function [grph] = image2Graph(grayImage, maxNodes, distanceThreshold)
     shortestDist = min(dist1,dist2)
     grph = graph(shortestDist < ...
         distanceThreshold) - eye(length(normalizedPoints));
-=======
-    points = [theta', rho'];
-    [normalizedTheta, shiftedRho] = houghPeakNormalization(theta, rho);
-    normalizedPoints = [normalizedTheta',shiftedRho'];
-    grph = graph((squareform(pdist(normalizedPoints)) < ...
-        distanceThreshold) - eye(length(normalizedPoints)));
->>>>>>> 766d00f6393b1db5d48c72b6711da5c00a28c868
+
 end
